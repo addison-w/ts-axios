a044c14af374a4aca4e3150cfb6c9da94a912664
Addison Wen <perhaps0660@gmail.com>
2020-05-14 04:06:50 +1000

    Added response type interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -21,3 +21,12 @@ export interface AxiosRequestConfig {
   params?: any
   headers?: any
 }
+
+export interface AxiosResponse {
+  data: any
+  status: number
+  statusText: string
+  headers: any
+  config: AxiosRequestConfig
+  request: any
+}
